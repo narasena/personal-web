4deabac4755167ecbdf4d94fc4d687c0cb141812
cursemaker <cursedsena@gmail.com>
2025-03-10 17:35:43 +0700

    fix portfolios UI px

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -37,7 +37,7 @@ const Portfolio = () => {
     <section id="portfolios" className="py-12 px-4 scroll-mt-20">
       <section className="bg-white dark:bg-gray-900 rounded-2xl">
         <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-          <div className="max-w-screen-md mb-8 lg:mb-16 min-w-full px-8">
+          <div className="max-w-screen-md mb-8 lg:mb-16 min-w-full px-2 sm:px-8">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">My Portfolios</h2>
             <p className="text-gray-500 sm:text-xl dark:text-gray-400">
               Welcome to my portfolio—a curated collection of projects where innovation meets technical excellence. Each
