e64482113bb8d418f8da0692cf4a13b8aed1873f
cursemaker <cursedsena@gmail.com>
2025-03-18 09:08:24 +0700

    add vercel speed-insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AnimatedGradientBounced from "@/components/animatedgradient.bounced.component";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,12 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased `}>
+        <SpeedInsights />
         <AnimatedGradientBounced />
         {children}
-
-        
       </body>
     </html>
   );
